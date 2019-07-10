df4994725be7c7296d0b85c7f91971fa3dfc6162
Danilo Martinez <danilo_98osorno@hotmail.com>
2019-07-10 12:40:04 -0500

    Se hacen mejoras en la vista

--- src/app/purchases/components/purhases/purchases.component.ts
+++ src/app/purchases/components/purhases/purchases.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./purchases.component.scss']
 })
 export class PurchasesComponent implements OnInit {
+  public purchases: any[];
+  public favorites: any[];
 
   constructor() { }
 
