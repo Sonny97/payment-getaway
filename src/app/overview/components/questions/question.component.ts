a78aea69a5d71e0e3204af68467eb0d08093a1cc
Danilo Martinez <danilo_98osorno@hotmail.com>
2019-07-17 11:33:26 -0500

    Se cambia la ruta para el detalle del producto, se agrega el valor del asin que permite ver el detalle del producto pero en amazon

--- src/app/overview/components/questions/question.component.ts
+++ src/app/overview/components/questions/question.component.ts
@@ -61,8 +61,16 @@ export class QuestionComponent implements OnInit {
    * openDetailProduct
    */
   public openDetailProduct(question) {
-    const url = `https://www.kiero.co/product-details?id=${question.Producto_Id}-${question.Titulo}`;
+    const url = `https://articulo.kiero.co/product-details/?id-${question.Producto_Id}--${question.Titulo}`;
     window.open(url);
+  }
+
+  /**
+   * goToAmazon: Let take the asin from amazon and go to amazon´s page
+   */
+  public goToAmazon(asin) {
+    const url = ` https://www.amazon.com/gp/product/${asin}/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1`;
+    window.open(url)
   }
 
   /**
